c8d12bad57974dc21f15aad0814454c9a2230b67
pveber <philippe.veber@gmail.com>
2011-10-10 22:24:25 +0200

    [ADD] minimal documentation

--- src/biocaml/eutils.mli
+++ src/biocaml/eutils.mli
@@ -1,7 +1,33 @@
+(** Entrez Utilities API
+
+    This modules provides a partial access to Entrez databases
+    such as Pubmed, Gene or Protein. The API proposed by the 
+    NCBI is based on HTTP requests, and this modules contains
+    a couple of functions to ease the construction of appropriate
+    URLs. It is thus of rather low-level; in particular, there is
+    no support for parsing the answers, which are simple strings.
+
+    Databases in Entrez can be seen as collections of records, each
+    record representing an object of the database. The basic usage
+    of the API is first to search a database with the esearch utility.
+    Given a query string, esearch will return a collection of 
+    identifiers. These identifiers are then used to fetch the actual
+    records with the efetch utility.
+*)
+
 type database = [
   `pubmed
 | `gene
 ]
+(** Represents available databases *)
+
+
+(** 4 Low level access 
+    
+    For a documentation of the parameters, see http://www.ncbi.nlm.nih.gov/books/NBK25499/
+*)
+
+
 
 val esearch_url : 
   ?retstart:int -> ?retmax:int -> 
@@ -11,8 +37,10 @@ val esearch_url :
   ?reldate:int ->
   ?mindate:string -> ?maxdate:string ->
   database -> string -> string
+(** Construction of esearch URLs. *)
 
-(** does not support more than 200 ids *)
 val efetch_url : 
   ?rettype:string -> ?retmode:string ->
   database -> string list -> string
+(** Construction of efetch URLs. Note that this access method 
+    does not support more than 200 ids *)
